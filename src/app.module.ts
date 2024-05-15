3cb004ec863265e9067c992ffec2dc1740d2216e
ewjee <ewjee@puzzleone.com>
2024-05-15 19:20:52 +0900

    :sparkles: Add user - controller , service, and repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal : true})],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({isGlobal : true}), UserModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
