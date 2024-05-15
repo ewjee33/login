88fb9fc72fe75a849660d8df8f515f9ed7ff395c
ewjee <ewjee@puzzleone.com>
2024-05-15 19:15:54 +0900

    :sparkles: Basic setups for the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { timeout } from 'connect-timeout';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableShutdownHooks();
+  app.use(timeout(process.env.TIMEOUT || '15s'));
+  await app.listen(process.env.PORTNUMBER);
 }
 bootstrap();
