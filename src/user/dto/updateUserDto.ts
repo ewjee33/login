bc11fa6884daed45ca4cf6b43fa18835969410b2
ewjee <ewjee@puzzleone.com>
2024-06-01 13:04:38 +0900

    :construction: Working on user.controller

--- src/user/dto/updateUserDto.ts
+++ src/user/dto/updateUserDto.ts
@@ -1,6 +1,18 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsObject , IsOptional} from 'class-validator';
 import { CreateUserDto } from './createUserDto';
 import { PartialType } from '@nestjs/mapped-types' 
 
 export class UpdateUserDto extends PartialType(CreateUserDto){
+    @IsObject()
+    @IsOptional()
+    $set?: {
+        email?: string;
+        password?: string;
+    };
+
+    @IsObject()
+    @IsOptional()
+    $inc?: {
+        score?: number;
+    };
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,68 +1,123 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dto/createUserDto';
 import { FindUserDto } from 'src/user/dto/findUserDto';
 import { UpdateUserDto } from 'src/user/dto/updateUserDto';
 import { DeleteUserDto } from 'src/user/dto/deleteUserDto';
+import { InjectConnection } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Logger } from 'src/utils/logger';
+import { UserService } from './user.service';
+import { SessionManager } from 'src/utils/sessionManager';
+import { Response , Request} from 'express';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService ,
+        private readonly logger: Logger,
+        private readonly sessionService: SessionManager , 
+        @InjectConnection() private readonly connection: mongoose.Connection,){}
     @Post()
-    create(@Body() createUserDto: CreateUserDto) : Promise<Response> {
+    async create(@Req() req: Request , @Body() createUserDto: CreateUserDto) : Promise<Response> {
+        const startTime : number = Date.now();
+        const logMessage = [ req.originalUrl , userId ];
+        const session = await this.sessionService.startSession();
+        let shouldCommit = false;
+        session.startTransaction();
         try {
-
+            return this.logger.logAndSend(req.url, startTime, res, { result: "success", reason: "invalid input" , group : null } , "error" , logMessage)
         } catch (error){
-
+            return this.logger.logAndSend(req.url, startTime, res, { result: "failure", reason: "invalid input" , group : null } , "error" , logMessage)
         } finally {
-
+          if (shouldCommit){
+              await session.commitTransaction();
+              } else {
+              await session.abortTransaction();
+              }
+          session.endSession();
         }
-        return 'This action adds a new user';
     }
 
     @Get()
-    findAll(@Body() findUserDto: FindUserDto) : Promise<Response> {
+    async findAll(@Req() req: Request , @Body() findUserDto: FindUserDto) : Promise<Response> {
+        const startTime : number = Date.now();
+        const logMessage = [ req.originalUrl , userId ];
+        const session = await this.sessionService.startSession();
+        let shouldCommit = false;
+        session.startTransaction();
         try {
 
         } catch (error){
 
         } finally {
-            
+            if (shouldCommit){
+                await session.commitTransaction();
+                } else {
+                await session.abortTransaction();
+                }
+            session.endSession();
         }
-        return 'This action finds a new user';
     }
 
     @Get(':id')
-    findOne(@Param('id') findUserDto: FindUserDto) : Promise<Response> {
+    async findOne(@Req() req: Request , @Param('id') findUserDto: FindUserDto) : Promise<Response> {
+        const startTime : number = Date.now();
+        const logMessage = [ req.originalUrl , userId ];
+        const session = await this.sessionService.startSession();
+        let shouldCommit = false;
+        session.startTransaction();
         try {
 
         } catch (error){
 
         } finally {
-            
+            if (shouldCommit){
+                await session.commitTransaction();
+                } else {
+                await session.abortTransaction();
+                }
+            session.endSession();
         }
-        return 'This action finds a new user';
     }
 
     @Put(':id')
-    update(@Param('id') id : number , @Body() updateUserDto: UpdateUserDto) : Promise<Response> {
+    async update(@Req() req: Request ,@Param('id') id : number , @Body() updateUserDto: UpdateUserDto) : Promise<Response> {
+        const startTime : number = Date.now();
+        const logMessage = [ req.originalUrl , userId ];
+        const session = await this.sessionService.startSession();
+        let shouldCommit = false;
+        session.startTransaction();
         try {
 
         } catch (error){
 
         } finally {
-            
+            if (shouldCommit){
+                await session.commitTransaction();
+                } else {
+                await session.abortTransaction();
+                }
+                session.endSession();
         }
-        return 'This action updates a new user';
     }
 
     @Delete(':id')
-    delete(@Param('id') id : number , @Body() deleteUserDto: DeleteUserDto) : Promise<Response>{
+    async delete(@Req() req: Request , @Param('id') id : number , @Body() deleteUserDto: DeleteUserDto) : Promise<Response>{
+        const startTime : number = Date.now();
+        const logMessage = [ req.originalUrl , userId ];
+        const session = await this.sessionService.startSession();
+        let shouldCommit = false;
+        session.startTransaction();
         try {
 
         } catch (error){
 
         } finally {
-            
+            if (shouldCommit){
+                await session.commitTransaction();
+                } else {
+                await session.abortTransaction();
+                }
+                session.endSession();
         }
-        return 'This action deletes a new user';
     }
 }
